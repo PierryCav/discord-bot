a75c87f2f03b873d7ab84b0116cae0f2afff8f72
PierryCav <104205666+PierryCav@users.noreply.github.com>
2022-04-29 23:33:50 -0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,11 +2,11 @@ const {GuildMember} = require('discord.js');
 
 module.exports = {
   name: 'stop',
-  description: 'Stop all songs in the queue!',
+  description: 'Pare todas as músicas na fila!',
   async execute(interaction, player) {
     if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
       return void interaction.reply({
-        content: 'You are not in a voice channel!',
+        content: '**( ❌ ) - Você não está em um canal de voz!**',
         ephemeral: true,
       });
     }
@@ -16,7 +16,7 @@ module.exports = {
       interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
     ) {
       return void interaction.reply({
-        content: 'You are not in my voice channel!',
+        content: '**( ❌ ) - Você não está no meu canal de voz!**',
         ephemeral: true,
       });
     }
@@ -25,9 +25,9 @@ module.exports = {
     const queue = player.getQueue(interaction.guildId);
     if (!queue || !queue.playing)
       return void interaction.followUp({
-        content: '❌ | No music is being played!',
+        content: '**(❌ ) - Nenhuma música está sendo tocada!**',
       });
     queue.destroy();
-    return void interaction.followUp({content: '🛑 | Stopped the player!'});
+    return void interaction.followUp({content: '**( 🛑 ) - Parei de tocar'});
   },
 };
