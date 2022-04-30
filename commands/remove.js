fea347b3d53955ede2a7fd0487245b7c26bae949
PierryCav <104205666+PierryCav@users.noreply.github.com>
2022-04-30 09:57:52 -0300

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -2,19 +2,19 @@ const {GuildMember} = require('discord.js');
 
 module.exports = {
   name: 'remove',
-  description: 'remove a song from the queue!',
+  description: 'remover uma música da fila!',
   options: [
     {
       name: 'number',
       type: 4, // 'INTEGER' Type
-      description: 'The queue number you want to remove',
+      description: 'O número da fila que você deseja remover',
       required: true,
     },
   ],
   async execute(interaction, player) {
     if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
       return void interaction.reply({
-        content: 'You are not in a voice channel!',
+        content: '**( ❌ ) - Você não está em um canal de voz!**',
         ephemeral: true,
       });
     }
@@ -24,20 +24,20 @@ module.exports = {
       interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
     ) {
       return void interaction.reply({
-        content: 'You are not in my voice channel!',
+        content: '**( ❌ ) - Você não está no meu canal de voz!**',
         ephemeral: true,
       });
     }
 
     await interaction.deferReply();
     const queue = player.getQueue(interaction.guildId);
-    if (!queue || !queue.playing) return void interaction.followUp({content: '❌ | No music is being played!'});
+    if (!queue || !queue.playing) return void interaction.followUp({content: '**( ❌ ) - Nenhuma música está sendo tocada!**'});
     const number = interaction.options.get('number').value - 1;
     if (number > queue.tracks.length)
-      return void interaction.followUp({content: '❌ | Track number greater than queue depth!'});
+      return void interaction.followUp({content: '**( ❌ ) - Número de faixa maior que a profundidade da fila!**'});
     const removedTrack = queue.remove(number);
     return void interaction.followUp({
-      content: removedTrack ? `✅ | Removed **${removedTrack}**!` : '❌ | Something went wrong!',
+      content: removedTrack ? `**( ✅ ) - Removido **${removedTrack}**!` : '**( ❌ ) - Algo deu errado!**',
     });
   },
 };
